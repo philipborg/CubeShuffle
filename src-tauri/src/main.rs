5c2bbaaef8d6f32132fd90b11154f67fb2d02c9b
philipborg <1827607+philipborg@users.noreply.github.com>
2022-07-03 16:23:11 +0200

    Update Tauri to 1.0.2 (#114)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,13 @@
 )]
 
 fn main() {
+    let context = tauri::generate_context!();
     tauri::Builder::default()
-        .run(tauri::generate_context!())
+        .menu(if cfg!(target_os = "macos") {
+            tauri::Menu::os_default(&context.package_info().name)
+        } else {
+            tauri::Menu::default()
+        })
+        .run(context)
         .expect("error while running tauri application");
 }
